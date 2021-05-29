e1e44ca34df5586a5db1fd82058cf24a355d489b
christinemkhachen <christinemkhachen@gmail.com>
2021-05-29 17:30:54 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+// 1) write a functions called divide
+// // 2) divide divides 2000 by 100
+// function divide() {
+//   return(2000 / 100);
+// }
+//let name = (parameter or placeholder same thing) *or no placeholder at all* => *with arrow functions u always need the arrow after the = * {
+//}
+//this is the syntax
+//let nameOfFunction = () => {
+//return // the thing we want the function to return
+//}
+
+let divide = () => {
+  return 2000 / 100;
+};
+let square = (a) => {
+  return a * a;
+};
+
+let add = (a, b) => {
+  return a + b;
+};
